690499a04ec59ea0a8fbd30d958bc2fb788c3015
davydova <alexander.davydov@ppb.com>
2023-03-06 02:20:58 +0100

    Header and Bottom navigation

---
+++ src/icons/Ecosystem.js
@@ -0,0 +1,20 @@
+import {Icon} from 'native-base';
+import {Path} from 'react-native-svg';
+
+<svg
+  width="20"
+  height="20"
+  viewBox="0 0 20 20"
+  fill="none"
+  xmlns="http://www.w3.org/2000/svg"
+/>;
+
+const EcosystemIcon = () => (
+  <Icon width="20" height="20" viewBox="0 0 20 20" fill="none">
+    <Path
+      d="M9.7 0L4.2 9H15.2L9.7 0ZM9.7 3.84L11.63 7H7.76L9.7 3.84ZM15.2 11C12.71 11 10.7 13.01 10.7 15.5C10.7 17.99 12.71 20 15.2 20C17.69 20 19.7 17.99 19.7 15.5C19.7 13.01 17.69 11 15.2 11ZM15.2 18C13.82 18 12.7 16.88 12.7 15.5C12.7 14.12 13.82 13 15.2 13C16.58 13 17.7 14.12 17.7 15.5C17.7 16.88 16.58 18 15.2 18ZM0.699997 19.5H8.7V11.5H0.699997V19.5ZM2.7 13.5H6.7V17.5H2.7V13.5Z"
+      fill="#91969F"
+    />
+  </Icon>
+);
+export default EcosystemIcon;
---
+++ src/screens/HomeScreen.tsx
@@ -0,0 +1,71 @@
+import {SafeAreaView, ScrollView, StyleSheet, Text, View} from 'react-native';
+import {
+  Colors,
+  DebugInstructions,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+import React, {PropsWithChildren} from 'react';
+import Header from '../components/Header';
+
+type SectionProps = PropsWithChildren<{
+  title: string;
+}>;
+
+function Section({children, title}: SectionProps): JSX.Element {
+  return (
+    <View style={styles.sectionContainer}>
+      <Text style={[styles.sectionTitle]}>{title}</Text>
+      <Text style={[styles.sectionDescription]}>{children}</Text>
+    </View>
+  );
+}
+
+export default function HomeScreen() {
+  return (
+    <SafeAreaView style={styles.safeArea}>
+      <Header />
+      <ScrollView style={styles.scrollView}>
+        <Section title="Step One">
+          Edit <Text style={styles.highlight}>App.tsx</Text> to change this
+          screen and then come back to see your edits.
+        </Section>
+        <Section title="See Your Changes">
+          <ReloadInstructions />
+        </Section>
+        <Section title="Debug">
+          <DebugInstructions />
+        </Section>
+        <Section title="Learn More">
+          Read the docs to discover what to do next:
+        </Section>
+        <LearnMoreLinks />
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+  safeArea: {
+    backgroundColor: '#DEE0E2',
+  },
+  scrollView: {
+    backgroundColor: Colors.white,
+  },
+});
